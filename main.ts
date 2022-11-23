1f4236a19a516f83586c74fc504765f24a77db72
josemcaba <96978309+josemcaba@users.noreply.github.com>
2022-11-24 00:49:30 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,27 +1,7 @@
 let alarma = 0
 let minutos = 1
 pins.servoWritePin(AnalogPin.P16, 0)
-basic.forever(function () {
-    if (ESP8266_IoT.wifiState(false)) {
-        basic.showIcon(IconNames.No)
-        ESP8266_IoT.initWIFI(SerialPin.P8, SerialPin.P12, BaudRate.BaudRate115200)
-        ESP8266_IoT.connectWifi("wiot", "a1b2c3d4")
-    }
-    basic.showIcon(IconNames.Yes)
-    if (Environment.PIR(DigitalPin.P2) && alarma == 0) {
-        pins.servoWritePin(AnalogPin.P16, 90)
-        alarma = 1
-        basic.showIcon(IconNames.Skull)
-        basic.pause(minutos * 2 * 60000)
-    } else {
-        if (alarma == 1) {
-            pins.servoWritePin(AnalogPin.P16, 0)
-            basic.pause(5000)
-        }
-        alarma = 0
-    }
-})
-loops.everyInterval(minutos * 60000, function () {
+loops.everyInterval(60000, function () {
     if (ESP8266_IoT.wifiState(true)) {
         basic.showIcon(IconNames.SmallHeart)
         ESP8266_IoT.connectThingSpeak()
@@ -38,5 +18,25 @@ loops.everyInterval(minutos * 60000, function () {
     }
     if (alarma == 1) {
         basic.showIcon(IconNames.Skull)
+    }
+})
+basic.forever(function () {
+    if (ESP8266_IoT.wifiState(false)) {
+        basic.showIcon(IconNames.No)
+        ESP8266_IoT.initWIFI(SerialPin.P8, SerialPin.P12, BaudRate.BaudRate115200)
+        ESP8266_IoT.connectWifi("wiot", "a1b2c3d4")
+    }
+    basic.showIcon(IconNames.Yes)
+    if (Environment.PIR(DigitalPin.P2) && alarma == 0) {
+        pins.servoWritePin(AnalogPin.P16, 90)
+        alarma = 1
+        basic.showIcon(IconNames.Skull)
+        basic.pause(120000)
+    } else {
+        if (alarma == 1) {
+            pins.servoWritePin(AnalogPin.P16, 0)
+            alarma = 0
+            basic.pause(5000)
+        }
     }
 })
